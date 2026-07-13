a81742971f383a1679894a3f7131de1161391dcd
Tanneru Astalakshmi <pandutanneeru6@gmail.com>
2026-07-13 20:03:45 +0530

    Refactor evaluation to use ARIMA for passenger forecasting
    Updated the evaluation method to use ARIMA model for forecasting and calculate MAE and RMSE based on predictions.

--- AirlinePassengerForecasting/src/evaluation.py
+++ AirlinePassengerForecasting/src/evaluation.py
@@ -1,16 +1,21 @@
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from statsmodels.tsa.arima.model import ARIMA
 
 class Evaluator:
 
     def evaluate(self, df):
-        y = df["Passengers"]
 
-        # dummy prediction (same as actual)
-        y_pred = y.copy()
+        train = df["Passengers"][:-12]
+        test = df["Passengers"][-12:]
 
-        mae = mean_absolute_error(y, y_pred)
-        rmse = np.sqrt(mean_squared_error(y, y_pred))
+        model = ARIMA(train, order=(5,1,0))
+        model_fit = model.fit()
+
+        predictions = model_fit.forecast(steps=12)
+
+        mae = mean_absolute_error(test, predictions)
+        rmse = np.sqrt(mean_squared_error(test, predictions))
 
         return {
             "MAE": mae,
